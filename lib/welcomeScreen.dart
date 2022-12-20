c449d652f13edc12a499ab614faccbd5bc4f0f5e
Bhumika Kalaru <bhumikakalaru@gmail.com>
2022-12-20 21:45:45 +0530

    Updated Welcome screen

--- lib/welcomeScreen.dart
+++ lib/welcomeScreen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:handsforhunger/donor.dart';
 import 'package:handsforhunger/ngo.dart';
-import 'package:sizer/sizer.dart';
 
 class WelcomeScreen extends StatefulWidget {
   @override
